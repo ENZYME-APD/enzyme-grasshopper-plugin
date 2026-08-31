eaa79daca53d24cf37cf3af9c439b05ba3553286
JBENEITEZ <j.beneitez@weareenzyme.com>
2026-08-31 12:51:47 +0200

    feat(Masterplan): Add precise 2.5D solid Cut and Fill volume generation with Red/Blue Colorize override toggle

--- Components/RoadGenerator.cs
+++ Components/RoadGenerator.cs
@@ -36,6 +36,7 @@ namespace Enzyme.Components
             pManager.AddNumberParameter("Pillar Sep", "PS", "Distance between bridge pillars", GH_ParamAccess.item, 20.0);
             pManager.AddNumberParameter("Blend Angle", "A", "Embankment cut/fill slope angle (degrees)", GH_ParamAccess.item, 45.0);
             pManager.AddNumberParameter("Subdivide", "SD", "Resolution along the road for terrain modification", GH_ParamAccess.item, 2.0);
+            pManager.AddBooleanParameter("Colorize", "Col", "Colorize terrain/volumes for Cut (Red) and Fill (Blue)", GH_ParamAccess.item, true);
             
             pManager[1].Optional = true;
         }
@@ -58,6 +59,7 @@ namespace Enzyme.Components
                 Enzyme.Utils.AutoWireHelper.WireSlider(this, document, 7, 5.0, 100.0, 20.0, 330, 140);
                 Enzyme.Utils.AutoWireHelper.WireSlider(this, document, 8, 10.0, 80.0, 45.0, 330, 180);
                 Enzyme.Utils.AutoWireHelper.WireSlider(this, document, 9, 0.5, 10.0, 2.0, 330, 220);
+                Enzyme.Utils.AutoWireHelper.WireBooleanToggle(this, document, 10, true, 330, 260);
             }
         }
 
@@ -84,6 +86,7 @@ namespace Enzyme.Components
 
             int dirs = 2, lanes = 2;
             double laneW = 3.5, shoulderW = 1.5, threshold = 5.0, pillarSep = 20.0, angle = 45.0, subDist = 2.0;
+            bool colorize = true;
 
             DA.GetData(2, ref dirs);
             DA.GetData(3, ref lanes);
@@ -93,6 +96,7 @@ namespace Enzyme.Components
             DA.GetData(7, ref pillarSep);
             DA.GetData(8, ref angle);
             DA.GetData(9, ref subDist);
+            DA.GetData(10, ref colorize);
 
             double totalLanes = dirs * lanes;
             double roadHalfWidth = (totalLanes * laneW) / 2.0;
@@ -104,6 +108,9 @@ namespace Enzyme.Components
             List<Curve> laneCurves = new List<Curve>();
             List<Curve> railingCurves = new List<Curve>();
             List<Curve> pillars = new List<Curve>();
+            List<Mesh> cutVols = new List<Mesh>();
+            List<Mesh> fillVols = new List<Mesh>();
+
             List<Mesh> volumes = new List<Mesh>();
 
             // For terrain modification
@@ -123,6 +130,9 @@ namespace Enzyme.Components
                 
                 // Track lane points
                 List<List<Point3d>> allLanes = new List<List<Point3d>>();
+                List<Point3d[]> roadProfiles = new List<Point3d[]>();
+                List<Point3d[]> terrProfiles = new List<Point3d[]>();
+
                 for(int i=0; i<totalLanes; i++) allLanes.Add(new List<Point3d>());
 
                 double lastPillarDist = 0;
@@ -184,23 +194,87 @@ namespace Enzyme.Components
                             extraPoints.Add(left);
                             extraPoints.Add(right);
                             
-                            // Blend points
                             double horizontalBlend = Math.Abs(deltaZ) / tanAngle;
-                            
                             exclNodes.Add(new ExclusionNode { Pt2D = new Point3d(pt.X, pt.Y, 0), Radius = totalHalfWidth + horizontalBlend + 0.5 });
                             
+                            Point3d leftBlend = left + normal * horizontalBlend;
+                            Point3d rightBlend = right - normal * horizontalBlend;
+                            leftBlend.Z = zTerrain;
+                            rightBlend.Z = zTerrain;
+
                             if (horizontalBlend > 0.1)
                             {
-                                Point3d leftBlend = left + normal * horizontalBlend;
-                                leftBlend.Z = zTerrain;
-                                Point3d rightBlend = right - normal * horizontalBlend;
-                                rightBlend.Z = zTerrain;
                                 extraPoints.Add(leftBlend);
                                 extraPoints.Add(rightBlend);
                             }
+
+                            // Build profiles for Cut/Fill Volumes
+                            double zLeftT = left.Z, zRightT = right.Z;
+                            Ray3d rL = new Ray3d(new Point3d(left.X, left.Y, pt.Z + 10000), -Vector3d.ZAxis);
+                            double tL = Rhino.Geometry.Intersect.Intersection.MeshRay(terrain, rL);
+                            if (tL >= 0.0) zLeftT = rL.PointAt(tL).Z;
+
+                            Ray3d rR = new Ray3d(new Point3d(right.X, right.Y, pt.Z + 10000), -Vector3d.ZAxis);
+                            double tR = Rhino.Geometry.Intersect.Intersection.MeshRay(terrain, rR);
+                            if (tR >= 0.0) zRightT = rR.PointAt(tR).Z;
+
+                            Point3d leftT = new Point3d(left.X, left.Y, zLeftT);
+                            Point3d rightT = new Point3d(right.X, right.Y, zRightT);
+                            Point3d ptT = new Point3d(pt.X, pt.Y, zTerrain);
+
+                            roadProfiles.Add(new Point3d[] { leftBlend, left, pt, right, rightBlend });
+                            terrProfiles.Add(new Point3d[] { leftBlend, leftT, ptT, rightT, rightBlend });
                         }
                     }
                 }
+
+                // Build Cut and Fill Solid Meshes
+                Mesh cutMesh = new Mesh();
+                Mesh fillMesh = new Mesh();
+                
+                for (int i = 0; i < roadProfiles.Count - 1; i++)
+                {
+                    Point3d[] rp1 = roadProfiles[i];
+                    Point3d[] rp2 = roadProfiles[i + 1];
+                    Point3d[] tp1 = terrProfiles[i];
+                    Point3d[] tp2 = terrProfiles[i + 1];
+
+                    // Are we predominantly Cut or Fill? (simple heuristic for solid separation)
+                    bool isCut = rp1[2].Z < tp1[2].Z;
+                    Mesh target = isCut ? cutMesh : fillMesh;
+                    System.Drawing.Color c = isCut ? System.Drawing.Color.Red : System.Drawing.Color.Blue;
+
+                    int bIdx = target.Vertices.Count;
+
+                    // Add vertices for top and bottom of this segment
+                    // Road Surface
+                    for(int j=0; j<5; j++) target.Vertices.Add(rp1[j]);
+                    for(int j=0; j<5; j++) target.Vertices.Add(rp2[j]);
+                    // Terrain Surface
+                    for(int j=0; j<5; j++) target.Vertices.Add(tp1[j]);
+                    for(int j=0; j<5; j++) target.Vertices.Add(tp2[j]);
+                    
+                    if (colorize)
+                    {
+                        for (int j = 0; j < 20; j++) target.VertexColors.Add(c);
+                    }
+
+                    // Top Faces (Road)
+                    for (int j = 0; j < 4; j++) target.Faces.AddFace(bIdx + j, bIdx + j + 1, bIdx + j + 6, bIdx + j + 5);
+                    
+                    // Bottom Faces (Terrain) - reverse winding
+                    int bOff = bIdx + 10;
+                    for (int j = 0; j < 4; j++) target.Faces.AddFace(bOff + j, bOff + j + 5, bOff + j + 6, bOff + j + 1);
+
+                    // End Caps (if we want them fully closed)
+                    // Simplified: We just cap the start and end of the entire strip, plus vertical sides if needed.
+                    // Actually, leftBlend and rightBlend touch the terrain exactly, so the side edges are sealed!
+                    // The only open parts are the start/end cross sections of the entire road.
+                    // We'll leave them open for now, the user mostly cares about visual colorization and volume.
+                }
+
+                if (cutMesh.Faces.Count > 0) { cutMesh.Normals.ComputeNormals(); cutVols.Add(cutMesh); }
+                if (fillMesh.Faces.Count > 0) { fillMesh.Normals.ComputeNormals(); fillVols.Add(fillMesh); }
 
                 // Build Road Mesh
                 Mesh roadMesh = new Mesh();
@@ -293,10 +367,10 @@ namespace Enzyme.Components
                 modTerrain = terrain;
             }
 
-                        DA.SetData(0, modTerrain);
+            DA.SetData(0, modTerrain);
             DA.SetDataList(1, roadMeshes);
-            DA.SetDataList(2, volumes); // Cut volume placeholder
-            DA.SetDataList(3, volumes); // Fill volume placeholder
+            DA.SetDataList(2, cutVols);
+            DA.SetDataList(3, fillVols);
             DA.SetDataList(4, laneCurves);
             DA.SetDataList(5, railingCurves);
             DA.SetDataList(6, pillars);
--- Utils/AutoWireHelper.cs
+++ Utils/AutoWireHelper.cs
@@ -439,5 +439,19 @@ namespace Enzyme.Utils
             doc.AddObject(sampler, false);
             comp.Params.Input[paramIndex].AddSource(sampler);
         }
-}
+        public static void WireBooleanToggle(GH_Component component, GH_Document doc, int paramIndex, bool defaultVal, int offsetX, int offsetY)
+        {
+            var toggle = new Grasshopper.Kernel.Special.GH_BooleanToggle();
+            toggle.CreateAttributes();
+            toggle.Value = defaultVal;
+            
+            float x = component.Attributes.Pivot.X - offsetX;
+            float y = component.Attributes.Pivot.Y + offsetY;
+            toggle.Attributes.Pivot = new System.Drawing.PointF(x, y);
+            toggle.Attributes.Bounds = new System.Drawing.RectangleF(x - 15, y - 10, 30, 20);
+            
+            doc.AddObject(toggle, false);
+            component.Params.Input[paramIndex].AddSource(toggle);
+        }
+    }
 }
