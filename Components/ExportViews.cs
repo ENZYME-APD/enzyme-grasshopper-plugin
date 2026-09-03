5ab25a27255e7a53b23e109774ac8de10dc11eda
JBENEITEZ <j.beneitez@weareenzyme.com>
2026-09-04 01:50:49 +0200

    feat: add autowiring to ExportViews component matching the UI layout

--- Components/ExportViews.cs
+++ Components/ExportViews.cs
@@ -286,7 +286,29 @@ namespace Enzyme.Components
             this.Message = $"EXPORT NAMED VIEWS\nTime: {sw.ElapsedMilliseconds} ms\n---\nVIEW: {viewText}\nLAYER: {lsText}\nSTYLE: {dsText}";
         }
 
-        // Add right-click menu option to auto-fill value list
+
+        public override void AddedToDocument(GH_Document document)
+        {
+            base.AddedToDocument(document);
+            if (this.Attributes == null) this.CreateAttributes();
+            
+            bool hasSources = false;
+            foreach (var param in this.Params.Input)
+                if (param.SourceCount > 0) { hasSources = true; break; }
+
+            if (!hasSources)
+            {
+                Enzyme.Utils.AutoWireHelper.WireButton(this, document, 0, 210, -180);
+                Enzyme.Utils.AutoWireHelper.WireFilePath(this, document, 2, "C:\\", 210, -120);
+                Enzyme.Utils.AutoWireHelper.WireToggle(this, document, 10, false, 210, 50);
+                Enzyme.Utils.AutoWireHelper.WireToggle(this, document, 11, false, 210, 80);
+                Enzyme.Utils.AutoWireHelper.WireToggle(this, document, 12, false, 210, 110);
+                Enzyme.Utils.AutoWireHelper.WireToggle(this, document, 13, true, 210, 140);
+
+                Enzyme.Utils.AutoWireHelper.WireOutputPanel(this, document, 1, 150, 0, 300, 300);
+            }
+        }
+
         public override void AppendAdditionalMenuItems(System.Windows.Forms.ToolStripDropDown menu)
         {
             base.AppendAdditionalMenuItems(menu);
