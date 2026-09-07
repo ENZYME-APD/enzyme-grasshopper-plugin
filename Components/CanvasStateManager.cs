e787db223976b25c13380b728551fda13f685555
JBENEITEZ <j.beneitez@weareenzyme.com>
2026-09-07 13:24:49 +0200

    feat: add right-click context menu to auto-fill load states ValueList

--- Components/CanvasStateManager.cs
+++ Components/CanvasStateManager.cs
@@ -1,3 +1,4 @@
+using System.Windows.Forms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -132,6 +133,57 @@ namespace Enzyme.Components
             return base.Read(reader);
         }
         
+
+        protected override void AppendAdditionalComponentMenuItems(ToolStripDropDown menu)
+        {
+            base.AppendAdditionalComponentMenuItems(menu);
+            Menu_AppendItem(menu, "Auto-fill Load ValueList", (s, e) => AutoFillValueList());
+        }
+
+        private void AutoFillValueList()
+        {
+            if (_states.Count == 0) return;
+
+            GH_Document doc = OnPingDocument();
+            if (doc == null) return;
+
+            var loadParam = this.Params.Input[3];
+            Grasshopper.Kernel.Special.GH_ValueList vl = null;
+
+            foreach (var source in loadParam.Sources)
+            {
+                if (source is Grasshopper.Kernel.Special.GH_ValueList v)
+                {
+                    vl = v;
+                    break;
+                }
+            }
+
+            bool isNew = false;
+            if (vl == null)
+            {
+                vl = new Grasshopper.Kernel.Special.GH_ValueList();
+                vl.CreateAttributes();
+                vl.Attributes.Pivot = new System.Drawing.PointF(this.Attributes.Pivot.X - 200, this.Attributes.Pivot.Y + 60);
+                vl.ListMode = Grasshopper.Kernel.Special.GH_ValueListMode.DropDown;
+                isNew = true;
+            }
+
+            vl.ListItems.Clear();
+            foreach (var key in _states.Keys)
+            {
+                vl.ListItems.Add(new Grasshopper.Kernel.Special.GH_ValueListItem(key, $"\"{key}\""));
+            }
+
+            if (isNew)
+            {
+                doc.AddObject(vl, false);
+                loadParam.AddSource(vl);
+            }
+
+            vl.ExpireSolution(true);
+        }
+
         public override void AddedToDocument(GH_Document document)
         {
             base.AddedToDocument(document);
