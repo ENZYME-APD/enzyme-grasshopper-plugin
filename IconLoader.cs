2e631c4374b95525f47f4f87edbd7177df29cf7e
JBENEITEZ <j.beneitez@weareenzyme.com>
2026-09-09 14:08:10 +0200

    fix: keep resource stream open for IconLoader to fix GDI+ silent fail on Mac

--- IconLoader.cs
+++ IconLoader.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Reflection;
 using System.Diagnostics;
@@ -7,29 +8,36 @@ namespace Enzyme
 {
     public static class IconLoader
     {
+        private static Dictionary<string, Bitmap> _iconCache = new Dictionary<string, Bitmap>();
+
         public static Bitmap Load(string iconName)
         {
+            if (string.IsNullOrEmpty(iconName)) return null;
+
+            if (_iconCache.TryGetValue(iconName, out Bitmap cachedIcon))
+            {
+                return cachedIcon;
+            }
+
             try
             {
                 var assembly = Assembly.GetExecutingAssembly();
                 var resourceName = $"Enzyme.Resources.{iconName}";
 
-                // Debug: Lists all resources
-                //string[] resources = assembly.GetManifestResourceNames();
-                //Trace.WriteLine($"Available resources: {string.Join(", ", resources)}");
-
                 Trace.WriteLine($"Using resource name: {resourceName}");
 
-                using (var stream = assembly.GetManifestResourceStream(resourceName))
+                // Do not dispose the stream! GDI+ Bitmaps require the underlying stream to remain open
+                // for the lifetime of the Bitmap, otherwise drawing them will fail.
+                var stream = assembly.GetManifestResourceStream(resourceName);
+                if (stream == null)
                 {
-                    if (stream == null)
-                    {
-                        Trace.WriteLine($"Resource stream is null for: {resourceName}");
-                        return null;
-                    }
-
-                    return new Bitmap(stream);
+                    Trace.WriteLine($"Resource stream is null for: {resourceName}");
+                    return null;
                 }
+
+                var bmp = new Bitmap(stream);
+                _iconCache[iconName] = bmp;
+                return bmp;
             }
             catch (Exception ex)
             {
