1129cf9c90fcd7451d0c2288d2a926b7badbec67
JBENEITEZ <j.beneitez@weareenzyme.com>
2026-09-14 00:43:47 +0200

    feat(Export): add auto-wire functionality to TurntableCamera inputs

--- Components/TurntableCamera.cs
+++ Components/TurntableCamera.cs
@@ -167,6 +167,39 @@ namespace Enzyme.Components
                 + "Perfect for architectural and product presentations, creating seamless rotating GIFs or video sequences of your Grasshopper geometry in its final display state.");
         }
 
+        public override void AddedToDocument(GH_Document document)
+        {
+            base.AddedToDocument(document);
+            bool hasSources = false;
+            foreach (var param in this.Params.Input)
+                if (param.SourceCount > 0) { hasSources = true; break; }
+
+            if (!hasSources)
+            {
+                AutoWireDefaultInputs();
+            }
+        }
+
+        protected override void AppendAdditionalComponentMenuItems(System.Windows.Forms.ToolStripDropDown menu)
+        {
+            base.AppendAdditionalComponentMenuItems(menu);
+            Menu_AppendItem(menu, "Auto-wire Default Inputs", (s, e) => AutoWireDefaultInputs());
+        }
+
+        private void AutoWireDefaultInputs()
+        {
+            GH_Document doc = OnPingDocument();
+            if (doc == null) return;
+
+            Enzyme.Utils.AutoWireHelper.WireSlider1Dec(this, doc, 1, 0.1, 500.0, 100.0, 200, -100);
+            Enzyme.Utils.AutoWireHelper.WireSlider1Dec(this, doc, 2, 0.0, 500.0, 50.0, 200, -80);
+            Enzyme.Utils.AutoWireHelper.WireSliderInt(this, doc, 3, 1, 360, 36, 200, -60);
+            Enzyme.Utils.AutoWireHelper.WirePanel(this, doc, 4, "C:\\Turntable", 200, -40, 150, 20);
+            Enzyme.Utils.AutoWireHelper.WireBooleanToggle(this, doc, 8, false, 200, 40);
+            Enzyme.Utils.AutoWireHelper.WireBooleanToggle(this, doc, 9, false, 200, 60);
+            Enzyme.Utils.AutoWireHelper.WireButton(this, doc, 10, 200, 80);
+        }
+
         protected override Bitmap Icon => Enzyme.IconLoader.Load("TurntableCamera.png"); // Uses fallback if missing
 
         public override Guid ComponentGuid => new Guid("4A9812DC-F41C-4B9A-A3E5-D9814C12D55B");
