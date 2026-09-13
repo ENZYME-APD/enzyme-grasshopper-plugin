16ffbdc08c242cf6a3a8a121f6205d1328e2a069
JBENEITEZ <j.beneitez@weareenzyme.com>
2026-09-13 22:04:11 +0200

    feat(State): add JSON import/export and clipboard features to CanvasStateManager

--- Components/CanvasStateManager.cs
+++ Components/CanvasStateManager.cs
@@ -36,16 +36,18 @@ namespace Enzyme.Components
             pManager.AddBooleanParameter("Load State", "Load", "Button to load the specified canvas state", GH_ParamAccess.item, false);
             pManager.AddTextParameter("Load Name", "L_Name", "Name of the state to load", GH_ParamAccess.item, "State1");
             pManager.AddBooleanParameter("Turn Off New Nodes", "OffNew", "If true, new nodes added after the state was saved will have preview disabled (hidden).", GH_ParamAccess.item, true);
+            pManager.AddTextParameter("Import JSON", "Import", "Optional JSON string from another State Manager to merge states.", GH_ParamAccess.item);
 
-            // Make text inputs optional so value lists without a selection don't break the component
             pManager[1].Optional = true;
             pManager[3].Optional = true;
+            pManager[5].Optional = true;
         }
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
             pManager.AddTextParameter("Available States", "States", "List of saved states", GH_ParamAccess.list);
             pManager.AddTextParameter("Info", "Info", "Status output", GH_ParamAccess.item);
+            pManager.AddTextParameter("Export JSON", "JSON", "Serialized JSON of all saved states. Plug this into 'Import JSON' of another State Manager to share states.", GH_ParamAccess.item);
         }
 
         private string _lastAction = "Idle";
@@ -166,6 +168,11 @@ namespace Enzyme.Components
 
             DA.SetDataList(0, _states.Keys.ToList());
             DA.SetData(1, "CANVAS STATE MANAGER\n\nHOW IT WORKS:\nSaves and restores the Enabled/Disabled (Locked) and Preview (Hidden) states of all components on the canvas.\n\nINTERPRETATION & IMPORTANCE:\nAllows you to swap between different visualization or computation modes instantly.");
+            
+            try {
+                string exportJson = Newtonsoft.Json.JsonConvert.SerializeObject(_states);
+                DA.SetData(2, exportJson);
+            } catch { }
 
             sw.Stop();
             this.Message = $"STATE MANAGER\nTime: {sw.ElapsedMilliseconds} ms\n---\n{_lastAction}\nSaved: {_states.Count}";
@@ -200,10 +207,29 @@ namespace Enzyme.Components
         }
         
 
-        protected override void AppendAdditionalComponentMenuItems(ToolStripDropDown menu)
+        protected override void AppendAdditionalComponentMenuItems(System.Windows.Forms.ToolStripDropDown menu)
         {
             base.AppendAdditionalComponentMenuItems(menu);
             Menu_AppendItem(menu, "Auto-fill Load ValueList", (s, e) => AutoFillValueList());
+            Menu_AppendSeparator(menu);
+            Menu_AppendItem(menu, "Copy States to Clipboard (JSON)", (s, e) => {
+                try {
+                    string json = Newtonsoft.Json.JsonConvert.SerializeObject(_states);
+                    System.Windows.Forms.Clipboard.SetText(json);
+                } catch { }
+            });
+            Menu_AppendItem(menu, "Paste States from Clipboard", (s, e) => {
+                try {
+                    string json = System.Windows.Forms.Clipboard.GetText();
+                    if (!string.IsNullOrWhiteSpace(json)) {
+                        var importedStates = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, NodeState>>>(json);
+                        if (importedStates != null) {
+                            foreach (var kvp in importedStates) { _states[kvp.Key] = kvp.Value; }
+                            this.ExpireSolution(true);
+                        }
+                    }
+                } catch { }
+            });
         }
 
         private void AutoFillValueList()
